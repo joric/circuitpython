28743fd2792b740741c50e1c52ab24693a1c027c
joric <joric@users.noreply.github.com>
2020-10-14 09:27:50 +0500

    first commit

---
+++ ports/nrf/boards/nrfmicro/mpconfigboard.mk
@@ -0,0 +1,15 @@
+USB_VID = 0x239A
+USB_PID = 0x80B4
+USB_PRODUCT = "nrfmicro"
+USB_MANUFACTURER = "joric"
+
+MCU_CHIP = nrf52840
+
+INTERNAL_FLASH_FILESYSTEM = 1
+
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BLE
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_HID
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_SSD1306
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_framebuf
